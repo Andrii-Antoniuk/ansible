3ab986fef39530f43e68bb06bd59c62d371ad1df
Andrii Antoniuk <andrii.antoniuk@scandiweb.com>
2024-07-15 13:21:04 +0200

    Made screen primary

--- screen/make-screens-work.sh
+++ screen/make-screens-work.sh
@@ -1,3 +1,3 @@
 xrandr --output eDP --scale 1x1 --panning 2560x1440 --pos 0x0 -r 120
-xrandr --output DisplayPort-6 --scale 1.5x1.5 --pos 2560x0 -r 120
+xrandr --output DisplayPort-6 --scale 1.5x1.5 --pos 2560x0 --primary -r 120
 xrandr --output DisplayPort-7 --pos 5440x0 --rotate right -r 60
