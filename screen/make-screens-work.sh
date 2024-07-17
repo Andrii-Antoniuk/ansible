e3fe6fa87181bdd4786ccb9da0c36e744588b783
Andrii Antoniuk <andrii.antoniuk@scandiweb.com>
2024-07-17 09:57:51 +0200

    Fixed screen script

--- screen/make-screens-work.sh
+++ screen/make-screens-work.sh
@@ -1,3 +1,2 @@
-xrandr --output eDP --scale 1x1 --panning 2560x1440 --pos 0x0 -r 120
-xrandr --output DisplayPort-6 --scale 1.5x1.5 --pos 2560x0 --primary -r 120
-xrandr --output DisplayPort-7 --pos 5440x0 --rotate right -r 60
+#!/bin/sh
+xrandr --output eDP --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --primary --mode 1920x1080 --scale 1.5x1.5 --pos 2560x0 --rotate normal --output DisplayPort-7 --mode 2560x1600 --pos 5440x0 --rotate right --output HDMI-1-0 --off
